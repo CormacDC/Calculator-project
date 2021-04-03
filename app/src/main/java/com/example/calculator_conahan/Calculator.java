065ebf19c9f4a8ea7af7d365d06b14d502062319
CormacDC <60583567+CormacDC@users.noreply.github.com>
2021-04-02 20:31:18 -0400

    added number & basic operator buttons

---
+++ app/src/main/java/com/example/calculator_conahan/Calculator.java
@@ -0,0 +1,19 @@
+package com.example.calculator_conahan;
+
+import java.math.*;
+
+public class Calculator {
+
+    public double calculate(String expression){
+
+
+
+    }
+
+    private double parse(String expression){
+
+        String[] split = expression.split("");
+
+    }
+
+}
